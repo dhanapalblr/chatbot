70f414e5dd4a67d4e85347f57a2407099ac77d92
dhanapalblr <dhanapalsing@gmail.com>
2021-05-26 14:29:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,31 +5,27 @@ from flask import jsonify
 
 from utils import *
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-  if request.method=='POST':
-    message = request.form['message']
-    p=pred(message,tokenizer,MODEL)  
-  return render_template('result.html',prediction = p)
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/utterance',methods=['POST'])
 def utterance():
-  if request.method=='POST':
-    message = request.args.get('utterance','')
+    if request.method=='POST':
+        m = request.get_json()
+        text=m['utterance']
 
-    p=pred(message,tokenizer,MODEL)  
+    output=[]
+    for j in ['iris','lcs','hiri']:
+        k=pred(text,j)
+        [output.append(h) for h in k]
 
-  return jsonify(p)
+    res={'intentList':sorted(output,key=lambda x: x['confidence'],reverse=True)}
+
+    return jsonify(res)
 
 if __name__ == "__main__":
 	#decide what port to run the app in
 	port = int(os.environ.get('PORT', 5000))
 	#run the app locally on the givn port
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port,debug=False)
 	#optional if we want to run in debugging mode
 	#app.run(debug=True)
